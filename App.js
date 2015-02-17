f3a20455f4ca5862946f3db751264dea0c5c9306
Sean <slevy@mediasignage.com>
2015-02-16 17:56:43 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,54 @@
+/**
+ App MediaSignage Inc (c) open source digital signage project.
+ Visit Github for license and docs: http://git.digitalsignage.com
+ @class App
+ @constructor
+ @return {Object} instantiated App
+ **/
+define(['underscore', 'jquery', 'backbone', 'bootstrap', 'backbone.controller', 'ComBroker', 'Lib'], function (_, $, Backbone, Bootstrap, backbonecontroller, ComBroker, Lib) {
+    var App = Backbone.Controller.extend({
+
+        // app init
+        initialize: function () {
+
+            window.BB = Backbone;
+            BB.globs = {};
+            BB.SERVICES = {};
+            BB.EVENTS = {};
+            BB.LOADING = {};
+            BB.CONSTS = {};
+            BB.globs['UNIQUE_COUNTER'] = 0;
+            BB.globs['RC4KEY'] = '226a3a42f34ddd778ed2c3ba56644315';
+            BB.lib = new Lib();
+            BB.lib.addBackboneViewOptions();
+            BB.lib.addBackboneCollectionSave();
+            BB.comBroker = new ComBroker();
+            BB.comBroker.name = 'AppBroker';
+            window.log = BB.lib.log;
+            $.ajaxSetup({cache: false});
+            $.ajaxSetup({
+                headers: {'Authorization': 'somePasswordHere'}
+            });
+
+            // define applications
+            BB.CONSTS.MAILWASP = 'mailWasp';
+            BB.CONSTS.EVERNODES = 'everNodes';
+
+            // internationalization
+            require(['localizer'], function () {
+                var lang = "en";
+                var opts = { language: lang, pathPrefix: "./_lang" };
+                $("[data-localize]").localize("local", opts);
+            });
+
+            // router init
+            require(['LayoutRouter'], function (LayoutRouter) {
+                var LayoutRouter = new LayoutRouter();
+                BB.history.start();
+                BB.comBroker.setService(BB.SERVICES['LAYOUT_ROUTER'], LayoutRouter);
+                LayoutRouter.navigate('authenticate/_/_', {trigger: true});
+            })
+        }
+    });
+    return App;
+});
---
+++ init.js
@@ -0,0 +1,86 @@
+/**
+ Require js initialization module definition file for StudioLite
+ @class Require init js
+ **/
+require.config({
+    waitSeconds: 25,
+    baseUrl: '/boilerplate/public/',
+    paths: {
+        'jquery': '_common/_jquery/std/jq1.9.1/jquery-1.9.1',
+        'backbone': '_common/_js/backbone/backbone',
+        'text': '_common/_js/requirejs/text',
+        'backbone.controller': '_common/_js/backbone-controller/backbone.controller',
+        'RC4': '_common/_js/rc4/RC4',
+        'Lib': '_libs/Lib',
+        'bootbox': '_common/_js/bootbox/bootbox',
+        'nouislider': '_common/_js/nouislider/jquery.nouislider',
+        'platform': '_common/_js/platform/platform',
+        'jsencrypt': '_common/_js/encrypts/jsencrypt',
+        'gibberish-aes': '_common/_js/encrypts/gibberish-aes',
+        'md5': '_common/_js/encrypts/md5',
+        'moment': '_common/_js/moment/moment',
+        'Cookie': '_common/_js/cookie/jquery.cookie',
+        'ComBroker': '_controllers/ComBroker',
+        'XDate': '_common/_js/xdate/xdate',
+        'simplestorage': '_common/_js/simplestorage/simpleStorage',
+        'underscore': '_common/_js/underscore/underscore',
+        'bootstrap': '_common/_js/bootstrap/js/bootstrap',
+        'socketio': '_common/_js/socketio/socketio',
+        'Elements': 'Elements',
+        'localizer': '_common/_js/localizer/dist/jquery.localize',
+        'LayoutRouter': '_controllers/LayoutRouter',
+        'MailWasp': '_controllers/MailWasp',
+        'EverNodes': '_controllers/EverNodes',
+        'StackView': '_libs/StackView'
+
+    },
+
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'backbone.controller': {
+            deps: ['underscore', 'jquery']
+        },
+        'LayoutRouter': {
+            deps: ['Elements', 'backbone.controller']
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'Cookie': {
+            deps: ['jquery'],
+            exports: 'cookie'
+        },
+        'socketio': {
+            exports: 'socketio'
+        },
+        'nouislider': {
+            exports: 'nouislider'
+        },
+        'ComBroker': {
+            deps: ['backbone', 'jquery']
+        },
+        'Elements': {
+            exports: 'Elements'
+        },
+        'bootbox': {
+            deps: ['jquery'],
+            exports: 'bootbox'
+        },
+        'RC4': {
+            exports: 'RC4'
+        },
+        'jsencrypt': {
+            exports: 'jsencrypt'
+        }
+    }
+});
+
+require(['App'], function (App) {
+    new App();
+});
